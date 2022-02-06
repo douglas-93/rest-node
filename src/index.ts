b2fb804093ec994c6f7e8ea85475c75055b26973
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2022-02-06 13:45:19 -0300

    Adicionando Typescript e incrementando a forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('executei')
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, resp: Response, next: NextFunction) => {
+    resp.status(200).send({ foo: "bar" });
+});
+
+app.listen(3000, () => {
+    console.log("Aplicação escutando na porta 3000");
+});
