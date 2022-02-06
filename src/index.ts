7d2926ba56185733a571f3e585e2f66bf582cf2c
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2022-02-06 19:02:50 -0300

    config basica e definicao de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
-app.get("/status", (req: Request, resp: Response, next: NextFunction) => {
-    resp.status(200).send({ foo: "bar" });
-});
+//Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Configurações da Rota
+app.use(statusRoute);
+app.use(usersRoute);
+
+// app.get("/status", (req: Request, resp: Response, next: NextFunction) => {
+//     resp.status(200).send({ foo: "Sucesso!" });
+// });
+
+// Configuração de inicialização
 app.listen(3000, () => {
     console.log("Aplicação escutando na porta 3000");
 });
